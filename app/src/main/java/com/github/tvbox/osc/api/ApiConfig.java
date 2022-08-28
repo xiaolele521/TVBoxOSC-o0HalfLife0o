fd40a3f54cd4a0c50e8dad8c2aa12a3ea2bc25ba
xiaolele521 <110508414+xiaolele521@users.noreply.github.com>
2022-08-28 12:18:58 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/xiaolelek/TVBOXjk/raw/branch/master/No.1.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
